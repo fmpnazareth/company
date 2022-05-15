1ce9002b6b69f761d95d04df0a99ca32bf8d67a1
fmpnazareth <Pass0628>
2022-05-15 09:55:02 -0300

    Ajusta banco de dados.

--- company/src/main/resources/data.sql
+++ company/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO tb_role (id, code, name) VALUES
-  (1, 'developer', 'Developer'),
-  (2, 'product_owner', 'Product Owner'),
-  (3, 'tester', 'Tester');
+INSERT INTO tb_role (name) VALUES
+  ('Developer'),
+  ('Product Owner'),
+  ('Tester');
